de46488df10bba6b3b0bb199b11deae232590cf0
ZXMushroom63 <zxmushroom63@gmail.com>
2025-12-16 20:47:04 +0200

    add my build env

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [
+        gnumake
+        libglvnd
+        alsa-lib
+        alsa-oss
+        portaudio
+        portmidi
+        jack2
+        xorg.libX11
+        xorg.libXinerama
+        xorg.libXext
+        xorg.libXcursor
+        curl
+        pkg-config
+        glib
+        libsecret
+        meson
+        ninja
+        fftw
+        fftwFloat
+        cmake
+        ftgl
+        freetype
+    ];
+}
