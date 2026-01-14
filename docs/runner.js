39eb05953340b5c182d8fe66d1bf947a90a174fe
ZXMushroom63 <zxmushroom63@gmail.com>
2026-01-14 22:14:40 +0200

    add live demo for gh pages

---
+++ docs/runner.js
@@ -0,0 +1,260 @@
+function detectWebGLContext() {
+    const canvas = document.createElement("canvas");
+    const gl = canvas.getContext("webgl");
+    return (gl instanceof WebGLRenderingContext)
+        ? "✓ Browser supports WebGL."
+        : "⚠︎ Error: Browser / device does not support WebGL.";
+}
+
+globalThis.VIAL_CHANNEL_COUNT = 2;
+globalThis.VIAL_TARGET_SAMPLERATE = 24000;
+globalThis.VIAL_AUDIO_STACK_SIZE_SAMPLES = 2048; //How much buffer to smooth over timing inconsistencies
+globalThis.VIAL_CLOCKSPEED_MULTIPLIER = 1.25; //Speed multiplier for how often the audio render loop checks if rendering is necessary
+globalThis.VIAL_BSIZE = 0; //is initialised automatically
+
+const pages = 9817; //64kb pages
+const memSize = pages * 64 * 1024;
+
+// float = 4b
+// if stereo, each packet is 1024 instead (obvi)
+// structure
+// DATA STORAGE = VIAL_CHANNEL_COUNT*audioQueueSize*??*4
+// POINTER STACK = (4byte pointer)*audioQueueSize*VIAL_CHANNEL_COUNT
+// CONSUMPTION = 1 byte
+// LOCK = 1 byte
+
+const SMEM = new WebAssembly.Memory({
+    initial: pages,
+    maximum: pages,
+    shared: true
+});
+
+const rootBuffer = SMEM.buffer;
+
+createVial({
+    canvas: document.querySelector("#canvas"),
+    wasmMemory: SMEM
+}).then(Vial => {
+    globalThis.Vial = Vial;
+});
+
+addEventListener("contextmenu", (e) => { e.preventDefault(); });
+function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+function ratelimit(func, mininterval) {
+    let lastSend = -1;
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        if ((Date.now() - lastSend) > mininterval) {
+            func.apply(this, args);
+            lastSend = Date.now();
+        } else {
+            timeout = setTimeout(() => {
+                func.apply(this, args);
+            }, mininterval * 2);
+        }
+    };
+}
+
+addEventListener("load", () => {
+    let inited = false;
+    document.querySelector("#webgl").innerText = detectWebGLContext() + "\n" + (crossOriginIsolated ? "✓ SharedArrayBuffer supported." : "⚠︎ Error: SharedArrayBuffer not supported. (try reloading?)");
+    document.querySelector("#init").addEventListener("click", () => {
+        if (inited || !Vial) {
+            return;
+        }
+        inited = true;
+        Vial._startApplication_Classic();
+        Vial._vialSetWindowSize(innerWidth * devicePixelRatio, innerHeight * devicePixelRatio);
+        let prevFrame = performance.now();
+        let audioTimeRef = -1;
+        let droppedPacketCountRef = 0;
+        document.querySelector("#bgvideo").remove();
+        document.querySelector("#init_panel").remove();
+        function renderLoop() {
+            const now = performance.now();
+            Vial._vialRedraw();
+            const post = performance.now();
+
+            setTimeout(renderLoop, Math.max(1 / 60, 1000 / 24 - (post - now)));
+            const audioResponseTime = (VIAL_BSIZE / VIAL_TARGET_SAMPLERATE * 1000).toFixed(1);
+            document.querySelector("#fps_counter").innerText = `${(1000 / (now - prevFrame)).toFixed(2)} FPS [💻${(1000 / (post - now)).toFixed(2)}] [🔊${(Vial.HEAPF64[audioTimeRef] || 0).toFixed(1)}ms / ${audioResponseTime}ms] [📦${VIAL_BSIZE}/${VIAL_TARGET_SAMPLERATE} : ${["MONO", "STEREO"][VIAL_CHANNEL_COUNT - 1] || "OTHER"}]`;
+
+            prevFrame = now;
+        }
+        renderLoop();
+        document.querySelector("#canvas").style.zIndex = 100;
+
+        setTimeout(async () => {
+            const audioContext = new AudioContext({
+                sampleRate: VIAL_TARGET_SAMPLERATE,
+                latencyHint: "playback"
+            });
+            await audioContext.audioWorklet.addModule("worklet.js");
+            const vialNode = new AudioWorkletNode(
+                audioContext,
+                "vial",
+                {
+                    numberOfOutputs: 1,
+                    outputChannelCount: [VIAL_CHANNEL_COUNT]
+                }
+            );
+            vialNode.port.onmessage = (ev) => {
+                if (ev.data.bufferSize) {
+                    globalThis.VIAL_BSIZE = ev.data.bufferSize;
+                    const audioStackSize = Math.ceil(VIAL_AUDIO_STACK_SIZE_SAMPLES / VIAL_BSIZE);
+                    console.log("Buffer size: ", VIAL_BSIZE);
+                    console.log(((audioStackSize * VIAL_BSIZE) / VIAL_TARGET_SAMPLERATE * 1000).toFixed(1) + "ms of audio latency.");
+
+                    audioTimeRef = Vial._setupAudioThread(VIAL_CHANNEL_COUNT, audioStackSize, VIAL_BSIZE, VIAL_CLOCKSPEED_MULTIPLIER) / 8;
+
+                    console.log("Attempting AudioWorklet init!");
+
+                    globalThis._dbgVialNode = vialNode;
+                    vialNode.port.postMessage({
+                        sharedArrBuf: rootBuffer,
+                        consumption: _V_AUDIO_CONSUMPTION_PTR,
+                        lockPtr: _V_AUDIO_LOCK_PTR,
+                        bufferStack: _V_AUDIO_PTRSTACK
+                    });
+
+                    console.log("Message posted.");
+                }
+            }
+
+
+            vialNode.connect(audioContext.destination);
+        }, 250);
+    });
+    const ev = {
+        lmb: false,
+        mmb: false,
+        rmb: false,
+        ctrlKey: false,
+        shiftKey: false,
+        altKey: false,
+        x: 0,
+        y: 0,
+        dbl: false,
+        wheel: 0,
+    }
+
+    function sendMouseEvent() {
+        if (!inited) {
+            return;
+        }
+        //console.log("sending mouse data.");
+        Vial._processMouseEvent(ev.lmb, ev.mmb, ev.rmb, ev.ctrlKey, ev.shiftKey, ev.altKey, Math.floor(ev.x * devicePixelRatio), Math.floor(ev.y * devicePixelRatio), ev.dbl, ev.wheel);
+        ev.wheel = 0;
+        ev.dbl = false;
+    }
+    const queueMouseEvent = ratelimit(sendMouseEvent, 1000 / 120);
+
+    console.log("Resistering input handlers.");
+    const canvas = document.querySelector("#canvas");
+    canvas.addEventListener("mousedown", (event) => {
+        if (event instanceof MouseEvent) {
+            event.preventDefault();
+            switch (event.button) {
+                case 0:
+                    ev.lmb = true;
+                    break;
+                case 1:
+                    ev.mmb = true;
+                    break;
+                case 2:
+                    ev.rmb = true;
+                    break;
+
+                default:
+                    break;
+            }
+            ev.ctrlKey = event.ctrlKey;
+            ev.altKey = event.altKey;
+            ev.shiftKey = event.shiftKey;
+            ev.x = event.layerX;
+            ev.y = event.layerY;
+            queueMouseEvent();
+        }
+    });
+    canvas.addEventListener("mouseout", () => {
+        event.preventDefault();
+        ev.lmb = false;
+        ev.mmb = false;
+        ev.rmb = false;
+        ev.ctrlKey = false;
+        ev.altKey = false;
+        ev.shiftKey = false;
+        queueMouseEvent();
+    });
+    canvas.addEventListener("dblclick", () => {
+        ev.dbl = true;
+        queueMouseEvent();
+    });
+    canvas.addEventListener("wheel", (e) => {
+        ev.wheel += Math.min(Math.max(-1, e.deltaY), 1);
+        queueMouseEvent();
+    });
+    canvas.addEventListener("mouseup", (event) => {
+        if (event instanceof MouseEvent) {
+            switch (event.button) {
+                case 0:
+                    ev.lmb = false;
+                    break;
+                case 1:
+                    ev.mmb = false;
+                    break;
+                case 2:
+                    ev.rmb = false;
+                    break;
+
+                default:
+                    break;
+            }
+            ev.ctrlKey = event.ctrlKey;
+            ev.altKey = event.altKey;
+            ev.shiftKey = event.shiftKey;
+            ev.x = event.layerX;
+            ev.y = event.layerY;
+            queueMouseEvent();
+        }
+    });
+    canvas.addEventListener("mousemove", (event) => {
+        if (event instanceof MouseEvent) {
+            ev.ctrlKey = event.ctrlKey;
+            ev.altKey = event.altKey;
+            ev.shiftKey = event.shiftKey;
+            ev.x = event.layerX;
+            ev.y = event.layerY;
+            queueMouseEvent();
+        }
+    });
+    const resizeHandler = () => {
+        console.log("Triggering resize!");
+        if (!inited) {
+            return;
+        }
+        Vial._vialSetWindowSize(innerWidth * devicePixelRatio, innerHeight * devicePixelRatio);
+    };
+
+    const debouncedHandler = debounce(resizeHandler, 850);
+    addEventListener("resize", () => {
+        debouncedHandler();
+    });
+});
+function getProgramComponents(programID, kNumShaders) {
+    kNumShaders ||= 21;
+    return {
+        "VertexShaderID": Math.floor(programID / kNumShaders),
+        "FragmentShaderID": programID % kNumShaders
+    };
+}
---
+++ docs/worklet.js
@@ -0,0 +1,101 @@
+class VialProcessor extends AudioWorkletProcessor {
+    constructor() {
+        super();
+        this.Lock = {
+            acquireLock: () => { },
+            releaseLock: () => { },
+        };
+        this.sharedMemory = null;
+        this.HEAPF32 = null;
+        this.HEAPU32 = null;
+        this.HEAPU8 = null;
+        this.lockPtr = -1;
+        this.consumptionPtr = -1;
+        this.bufferPtr = -1;
+
+        // i sincerely hate W3C for replacing the beautiful, clean, scriptprocessor with this piece of junk
+        // wdym i cant specify my own buffer size
+        // wdym there is *no* official way to check the buffer size
+        // who designed ts?? are they dumb???
+        this.sentBufferSize = false;
+
+        // AND, to make it EVEN WORSE, the buffer size can just *change*
+        // excuse me? I'm working with efficient webassembly here!
+        // I allocate memory *once*, not every time your message is called.
+        // and you want me to resize my buffers every frame? this is why web apps have a reputation of being slow!
+        this.confirmedBufferSize = -1;
+
+        this.port.onmessage = this.handleInitMessage.bind(this);
+        console.log("Ready for message.");
+    }
+
+    handleInitMessage(event) {
+        console.log("Received event!!!", event);
+        const { sharedArrBuf, consumption, lockPtr, bufferStack } = event.data;
+        console.log("Worklet Initialised! Data: ", event.data);
+        this.sharedMemory = sharedArrBuf;
+        this.HEAPF32 = new Float32Array(sharedArrBuf);
+        this.HEAPU32 = new Uint32Array(sharedArrBuf);
+        this.HEAPU8 = new Uint8Array(sharedArrBuf);
+        this.consumptionPtr = consumption;
+        this.bufferPtr = bufferStack;
+        this.lockPtr = lockPtr;
+        const self = this;
+        
+        this.Lock.acquireLock = function () {
+            while (Atomics.compareExchange(self.HEAPU8, lockPtr, 0, 1) !== 0) {
+                // spinlock
+            }
+        }
+
+        this.Lock.releaseLock = function () {
+            Atomics.store(self.HEAPU8, lockPtr, 0);
+        }
+    };
+
+    process(inputList, outputList, parameters) {
+        const bSize = outputList[0][0].length;
+        if (!this.sentBufferSize) {
+            this.confirmedBufferSize = bSize;
+            this.sentBufferSize = true;
+            this.port.postMessage({
+                bufferSize: bSize
+            });
+            return true;
+        }
+
+        if (this.confirmedBufferSize !== bSize) {
+            console.error("The AudioContext went ahead and resized the buffer without asking :(");
+            console.error("Old: ", this.confirmedBufferSize, "; New: ", bSize);
+            return false; //kill the audio processor
+        }
+        
+        if (!this.sharedMemory) {
+            return true;
+        }
+        this.Lock.acquireLock(); //profiled, almost always takes less than 1ms
+
+        const tOut = outputList[0];
+        const HEAPU32 = this.HEAPU32;
+        const HEAPF32 = this.HEAPF32;
+        const HEAPU8 = this.HEAPU8;
+        try {
+            const consumption = HEAPU8[this.consumptionPtr];
+            const ptr = HEAPU32[this.bufferPtr / 4 + consumption];
+            if (!ptr) {
+                return true;
+            }
+            for (let c = 0; c < tOut.length; c++) {
+                const f32 = tOut[c];
+                const dataPtr = HEAPU32[ptr / 4 + c] / 4;
+                f32.set(HEAPF32.subarray(dataPtr, dataPtr + this.confirmedBufferSize));
+            }
+            HEAPU8[this.consumptionPtr] = consumption + 1;
+        } finally {
+            this.Lock.releaseLock();
+        }
+        return true;
+    }
+}
+
+registerProcessor("vial", VialProcessor);
