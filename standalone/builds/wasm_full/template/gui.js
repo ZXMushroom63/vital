a5aa8eb8b824bf0801b8edc3c608ce1fed1d19bf
ZXMushroom63 <zxmushroom63@gmail.com>
2026-01-16 20:33:33 +0200

    make launcher remember options

--- standalone/builds/wasm_full/template/gui.js
+++ standalone/builds/wasm_full/template/gui.js
@@ -2,6 +2,11 @@ addEventListener("load", ()=>{
     document.querySelectorAll("[data-target][data-value]").forEach(node => {
         const value = parseFloat(node.getAttribute("data-value"));
         const targetProp = node.getAttribute("data-target");
+        const storageKey = `vialLauncherProperty->${targetProp}`;
+        const stored = localStorage.getItem(storageKey);
+        if (stored) {
+            globalThis[targetProp] = parseFloat(stored);
+        }
         if (!node.innerText.trim()) {
             node.innerText = value + (node.getAttribute("data-suffix") || "");
         }
@@ -12,6 +17,7 @@ addEventListener("load", ()=>{
             document.querySelectorAll(`[data-target=${targetProp}]`).forEach(x => x.classList.remove("selected"));
             node.classList.add("selected");
             globalThis[targetProp] = value;
+            localStorage.setItem(storageKey, value);
         });
     });
 });
